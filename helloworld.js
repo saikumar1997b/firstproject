53985a3ad29bce49d72b31e90c4972342bf025e8
sai kumar <skumram155@gmail.com>
2025-10-23 00:34:28 -0500

    check my code

--- helloworld.js
+++ helloworld.js
@@ -2,3 +2,9 @@
 
 console.log('Hello, world!');
 console.log('This is a simple Node.js script.');
+const firstName = 'jonas';
+const job = 'Teacher';
+const birthYear = 1990 ;
+const year = 2031 ;
+const jonas = `I'm ${firstName}, a ${year - birthYear} Year old ${job}!`;
+console.log(jonas);
