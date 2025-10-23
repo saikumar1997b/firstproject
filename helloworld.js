8a11b4f4c4198ae40e8220a84ac920ca6d4dcd50
sai kumar <skumram155@gmail.com>
2025-10-23 00:42:45 -0500

    check for details

--- helloworld.js
+++ helloworld.js
@@ -7,4 +7,4 @@ const job = 'Teacher';
 const birthYear = 1990 ;
 const year = 2031 ;
 const jonas = `I'm ${firstName}, a ${year - birthYear} Year old ${job}!`;
-console.log(jonas);
+console.log(smith);
